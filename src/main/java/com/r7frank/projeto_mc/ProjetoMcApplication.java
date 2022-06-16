6ba73f2fcd50101288f31d840a6f235151f16a3c
r7frank <r7frank@hotmail.com>
2022-06-16 15:21:00 -0300

    criacao do projeto

---
+++ src/main/java/com/r7frank/projeto_mc/ProjetoMcApplication.java
@@ -0,0 +1,13 @@
+package com.r7frank.projeto_mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r7frank/projeto_mc/ProjetoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r7frank.projeto_mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
