5eaf3226c3c3ecee460e724a3806c098400132c4
r7frank <r7frank@hotmail.com>
2022-06-18 15:31:02 -0300

    Criacao de operacao de instanciacao

--- src/main/java/com/r7frank/projeto_mc/ProjetoMcApplication.java
+++ src/main/java/com/r7frank/projeto_mc/ProjetoMcApplication.java
@@ -1,13 +1,31 @@
 package com.r7frank.projeto_mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoMcApplication {
+import com.r7frank.projeto_mc.domain.Categoria;
+import com.r7frank.projeto_mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
