e76c64845deeb6421035a7b60ca8d1db934ca9d8
r7frank <r7frank@hotmail.com>
2022-06-18 18:16:34 -0300

    Classe Produto criada e associada com a classe Categoria - relacao muitos para muitos

--- src/main/java/com/r7frank/projeto_mc/ProjetoMcApplication.java
+++ src/main/java/com/r7frank/projeto_mc/ProjetoMcApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.r7frank.projeto_mc.domain.Categoria;
+import com.r7frank.projeto_mc.domain.Produto;
 import com.r7frank.projeto_mc.repositories.CategoriaRepository;
+import com.r7frank.projeto_mc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoMcApplication.class, args);
 	}
@@ -25,7 +31,19 @@ public class ProjetoMcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
