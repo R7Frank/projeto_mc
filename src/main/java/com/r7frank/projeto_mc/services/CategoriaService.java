e6384b0a97ed2d94be279283a1cd3b07d1ddd9c4
r7frank <r7frank@hotmail.com>
2022-06-19 13:40:51 -0300

    Tratamentos: referencia ciclica e endopoint categorias/id

--- src/main/java/com/r7frank/projeto_mc/services/CategoriaService.java
+++ src/main/java/com/r7frank/projeto_mc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.r7frank.projeto_mc.domain.Categoria;
 import com.r7frank.projeto_mc.repositories.CategoriaRepository;
+import com.r7frank.projeto_mc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+				+ ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
